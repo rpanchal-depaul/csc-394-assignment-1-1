f17a1f60985c25bd9cfaaab811f73ea60a6ee50a
Rohan Panchal <rpancha2@depaul.edu>
2022-04-07 17:19:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ const Pool = require('pg').Pool
 
 var connectionParams = null;
 if (process.env.DATABASE_URL != null) {
-    connectionParams = { connectionString: process.env.DATABASE_URL + "?sslmode=require" }
+    connectionParams = {
+        connectionString: process.env.DATABASE_URL + "?sslmode=require",
+        ssl: { rejectUnauthorized: false }
+    }
 } else {
     connectionParams = {
         user: 'api_user',
