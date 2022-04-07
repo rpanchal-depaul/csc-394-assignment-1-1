e3effe3e01d4360cb4b57853007a0bba2d6e70e9
Rohan Panchal <rpancha2@depaul.edu>
2022-04-07 16:29:43 -0500

    Added templating engine as well as database integration!

--- index.js
+++ index.js
@@ -1,14 +1,70 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000
+
+// Set the view engine for the express app
+app.set("view engine", "jade")
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
+
+
+// Database
+const Pool = require('pg').Pool
+
+
+const connectionParams = process.env.DATABASE_URL || {
+
+    user: 'api_user',
+    host: 'localhost',
+    database: 'api',
+    password: 'password',
+    port: 5432
+}
+
+const pool = new Pool({
+  user: 'api_user',
+  host: process.env.DATABASE_URL || 'localhost',
+  database: 'api',
+  password: 'password',
+  port: 5432
+})
+
 
 app.get('/', (req, res) => {
 
   console.log('Accept: ' + req.get('Accept'))
 
-  res.send('<h1>Hello World!</h1>')
+  pool.query('SELECT VERSION()', (err, version_results) => {
+    console.log(err, version_results.rows)
 
-  console.log('Content-Type: ' + res.get('Content-Type'))
+    pool.query('SELECT * FROM team_members', (err, team_members_results) => {
+      console.log(err, team_members_results)
+
+      res.render('index', {
+                            teamNumber: 1,
+                            databaseVersion: version_results.rows[0].version, 
+                            teamMembers: team_members_results.rows 
+                          })
+      
+      console.log('Content-Type: ' + res.get('Content-Type'))
+    })  
+  })
+})
+
+app.post('/', (req, res) => {
+    
+    pool.query(`INSERT INTO team_members (first_name, last_name) VALUES ('${req.body.first_name}', '${req.body.last_name}')`, (err, result) => {
+        
+        console.log(err, result)
+        
+        res.redirect('/')
+    })
 })
 
 app.listen(port, () => {
